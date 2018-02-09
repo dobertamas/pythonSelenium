5b8131314a44e8c66248f657886927c6df621793
Tamas Dober <tamas.dober@mobileforming.com>
2018-02-09 12:33:11 -0800

    adding nose test library

--- tests/acceptance/locators/home_page.py
+++ tests/acceptance/locators/home_page.py
@@ -1,7 +1,9 @@
 from selenium.webdriver.common.by import By
 
+from tests.acceptance.locators.base_page import BasePageLocator
 
-class HomePageLocators:
+
+class HomePageLocators(BasePageLocator):
     USERNAME = (By.NAME, 'username')
     PASSWORD = (By.NAME, 'password')
     SIGN_IN_BUTTON = (By.NAME, 'signin')
--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -1,5 +1,6 @@
 from behave import *
 from selenium import webdriver
+from nose.tools import *
 
 from tests.acceptance.page_model.home_page import HomePage
 
@@ -19,8 +20,9 @@ def step_impl(context):
 @then('I am on the console/config/list page')
 def step_impl(context):
     expected_url = 'http://localhost:8084/console/config/list'
-    print(expected_url)
+    # print(expected_url)
     current_url = context.driver.current_url
-    print(current_url)
-    assert context.driver.current_url == expected_url
+    # print(current_url)
+    # assert context.driver.current_url == expected_url
+    assert_equal(current_url, expected_url)
     context.driver.close()
